9f19669488b7a4a60472cc8f500670d717edb0ac
William Bugenis <wbugenis@gmail.com>
2020-12-06 03:23:39 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,22 @@
+def using_push(array, string)
+  array.push(string)
+end
+
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop 
+end
+
+def pop_with_args(array)
+  array.pop(2)
+end
+
+def using_shift(array)
+  array.using_shift
+end
+
+def shift_with_args(array)
+  array.shift
